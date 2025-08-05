1b893eaa87cf3032cba39cac0b955106c47ef989
Laiba Khan <codewithlaiba28@gmail.com>
2025-08-05 13:52:28 +0500

    add

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-  const openBtn = document.querySelector('.small_button');
-  const closeBtn = document.querySelector('.close_button');
-  const navBar = document.querySelector('.nav-bar');
+const openBtn = document.querySelector('.small_button');
+const closeBtn = document.querySelector('.close_button');
+const navBar = document.querySelector('.nav-bar');
 
-  openBtn.addEventListener('click', () => {
-    navBar.classList.add('show');
+openBtn.addEventListener('click', () => {
+  navBar.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  navBar.classList.remove('show');
+});
+
+
+
+
+
+
+const cards = document.querySelectorAll('.card');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // immediate trigger
+      observer.unobserve(entry.target); // animate only once
+    }
   });
+}, {
+  threshold: 0.2
+});
 
-  closeBtn.addEventListener('click', () => {
-    navBar.classList.remove('show');
-  });
-
-
-
+cards.forEach(card => observer.observe(card));
 
 
 
-  const cards = document.querySelectorAll('.card');
-
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show'); // immediate trigger
-        observer.unobserve(entry.target); // animate only once
-      }
-    });
-  }, {
-    threshold: 0.2
-  });
-
-  cards.forEach(card => observer.observe(card));
-
-
-
-  const serviceCards = document.querySelectorAll('.services_card');
+const serviceCards = document.querySelectorAll('.services_card');
 
 const observerServices = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
@@ -47,4 +47,3 @@ serviceCards.forEach(card => observerServices.observe(card));
 
 
 
-  
